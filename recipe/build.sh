c063d344a8ce2658e981083b58f5a2e25373323f
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-03-02 19:06:07 +0100

    Adds Apple Silicon support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-cmake -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX}
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+  cmake -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX}
+else
+  cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX}
+fi
 ninja -C build install
