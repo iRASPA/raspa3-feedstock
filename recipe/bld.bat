6127f33eb1fcc86ddad1dfb43c9e7d1761a59a4e
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-03-14 06:29:01 +0100

    windows and linux arm support (#10)
    * try aarch64 support
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.46.1, and conda-forge-pinning 2025.03.05.16.19.52
    
    * remove 64-bits lapack requirement
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.46.1, and conda-forge-pinning 2025.03.05.16.19.52
    
    * remove 64-bits lapack requirement
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.46.1, and conda-forge-pinning 2025.03.05.16.19.52
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.46.1, and conda-forge-pinning 2025.03.05.16.19.52
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.46.1, and conda-forge-pinning 2025.03.06.18.23.43
    
    * attempt windows and linux arm support
    
    * use vs2022 for windows
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.47.0, and conda-forge-pinning 2025.03.12.11.34.20
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.47.0, and conda-forge-pinning 2025.03.12.11.34.20
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.47.0, and conda-forge-pinning 2025.03.12.19.21.36
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.47.0, and conda-forge-pinning 2025.03.13.13.09.00
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.47.0, and conda-forge-pinning 2025.03.13.13.09.00
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.47.0, and conda-forge-pinning 2025.03.13.13.09.00
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.47.0, and conda-forge-pinning 2025.03.13.13.09.00

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-cmake ${CMAKE_ARGS} -G "Ninja Multi-Config" -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=%PREFIX%
+cmake -B build -G "Ninja Multi-Config" --preset windows_conda_raspa3 -DCMAKE_INSTALL_PREFIX=%PREFIX% 
 if errorlevel 1 exit 1
 ninja -C build install
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
-  cmake -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF
+  if [[ "${target_platform}" == linux-64 ]]; then
+    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} 
+  elif [[ "${target_platform}" == linux-ppc64le ]]; then
+    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF
+  elif [[ "${target_platform}" == linux-aarch64 ]]; then
+    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF 
+  elif [[ "${target_platform}" == osx-* ]]; then
+    cmake ${CMAKE_ARGS} -B build --preset mac_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX}
+  fi
 else
-  cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_OSX_ARCHITECTURES="arm64" -DBUILD_TESTING=OFF
+  if [[ "${target_platform}" == "linux"* ]]; then
+    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX}
+  elif  [[ "${target_platform}" == osx-* ]]; then
+    cmake ${CMAKE_ARGS} -B build --preset mac_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_OSX_ARCHITECTURES="arm64"
+  fi
 fi
 ninja -C build install -v
