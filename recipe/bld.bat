9dddc7605eb1f1bd73b831d3af5fbb62969da17f
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-05-08 01:51:09 +0200

    Update for 3.0.5 and opencl (#16)
    Co-authored-by: David Dubbeldam <d.dubbeldam@uva.nl>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-cmake -B build -G "Ninja Multi-Config" --preset windows_conda_raspa3 -DCMAKE_INSTALL_PREFIX=%PREFIX% 
+cmake -B build -G "Ninja Multi-Config" --preset windows_conda_raspa3 -DCMAKE_INSTALL_PREFIX=%PREFIX%  -DCMAKE_POLICY_VERSION_MINIMUM=3.32
 if errorlevel 1 exit 1
 ninja -C build install
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,17 +2,17 @@
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
   if [[ "${target_platform}" == linux* ]]; then
-    cmake -B build --preset=linux_conda_raspa3
+    cmake -B build --preset=linux_conda_raspa3 -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   elif [[ "${target_platform}" == osx-* ]]; then
-    cmake -B build --preset=mac_conda_raspa3
+    cmake -B build --preset=mac_conda_raspa3 -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   fi
 else
-  if [[ "${target_platform}" == linux-aarch64 ]]; then
-    cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_CXX_COMPILER_AR="$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-ar" -DCMAKE_CXX_COMPILER_RANLIB="$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-ranlib"
+  if [[ "${target_platform}" == linux-aarch64 ]]; then 
+    cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_CXX_COMPILER_AR="$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-ar" -DCMAKE_CXX_COMPILER_RANLIB="$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-ranlib" -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   elif [[ "${target_platform}" == linux-ppc64le ]]; then
-    cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_CXX_COMPILER_AR="$BUILD_PREFIX/bin/powerpc64le-conda_cos7-linux-gnu-ar" -DCMAKE_CXX_COMPILER_RANLIB="$BUILD_PREFIX/bin/powerpc64le-conda_cos7-linux-gnu-ranlib"
+    cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_CXX_COMPILER_AR="$BUILD_PREFIX/bin/powerpc64le-conda_cos7-linux-gnu-ar" -DCMAKE_CXX_COMPILER_RANLIB="$BUILD_PREFIX/bin/powerpc64le-conda_cos7-linux-gnu-ranlib" -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   elif  [[ "${target_platform}" == osx-* ]]; then
-    cmake -B build --preset=mac_conda_raspa3 ${CMAKE_ARGS}
+    cmake -B build --preset=mac_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   fi
 fi
 
