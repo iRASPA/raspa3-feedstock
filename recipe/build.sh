0d55ea8bd5262cdd8a678b185133c3b11d6d0c7c
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-07-28 21:41:53 +0200

    chore: release v3.0.8 (#25)
    * chore: release v3.0.8
    
    * MNT: Re-rendered with conda-build 25.5.0, conda-smithy 3.51.1, and conda-forge-pinning 2025.07.26.20.42.37
    
    ---------
    
    Co-authored-by: David Dubbeldam <d.dubbeldam@uva.nl>

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,9 +8,9 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
   fi
 else
   if [[ "${target_platform}" == linux-aarch64 ]]; then 
-    cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_CXX_COMPILER_AR="$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-ar" -DCMAKE_CXX_COMPILER_RANLIB="$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-ranlib" -DCMAKE_POLICY_VERSION_MINIMUM=3.32
+    cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS}  -DCMAKE_CXX_COMPILER_AR="$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-ar" -DCMAKE_CXX_COMPILER_RANLIB="$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-ranlib" -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   elif [[ "${target_platform}" == linux-ppc64le ]]; then
-    cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_CXX_COMPILER_AR="$BUILD_PREFIX/bin/powerpc64le-conda_cos7-linux-gnu-ar" -DCMAKE_CXX_COMPILER_RANLIB="$BUILD_PREFIX/bin/powerpc64le-conda_cos7-linux-gnu-ranlib" -DCMAKE_POLICY_VERSION_MINIMUM=3.32
+    cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_CXX_COMPILER_AR="$BUILD_PREFIX/bin/powerpc64le-conda-linux-gnu-ar" -DCMAKE_CXX_COMPILER_RANLIB="$BUILD_PREFIX/bin/powerpc64le-conda-linux-gnu-ranlib" -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   elif  [[ "${target_platform}" == osx-* ]]; then
     cmake -B build --preset=mac_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   fi
