9831090c9984d7b1455ec96262dc138ee57ff9f7
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-10-29 07:15:14 +0100

    chore: release v3.0.16 (#39)
    * chore: release v3.0.16
    
    * MNT: Re-rendered with conda-smithy 3.52.3 and conda-forge-pinning 2025.10.18.16.08.03
    
    Other tools:
    - conda-build 25.9.0
    - rattler-build 0.48.1
    - rattler-build-conda-compat 1.4.9
    
    * MNT: Re-rendered with conda-smithy 3.52.3 and conda-forge-pinning 2025.10.20.16.06.25
    
    Other tools:
    - conda-build 25.9.0
    - rattler-build 0.48.1
    - rattler-build-conda-compat 1.4.9
    
    * MNT: Re-rendered with conda-smithy 3.52.3 and conda-forge-pinning 2025.10.20.16.06.25
    
    Other tools:
    - conda-build 25.9.0
    - rattler-build 0.48.1
    - rattler-build-conda-compat 1.4.9
    
    * MNT: Re-rendered with conda-smithy 3.52.3 and conda-forge-pinning 2025.10.20.16.06.25
    
    Other tools:
    - conda-build 25.9.0
    - rattler-build 0.48.1
    - rattler-build-conda-compat 1.4.9
    
    * MNT: Re-rendered with conda-smithy 3.52.3 and conda-forge-pinning 2025.10.22.10.12.30
    
    Other tools:
    - conda-build 25.9.0
    - rattler-build 0.48.1
    - rattler-build-conda-compat 1.4.9
    
    * MNT: Re-rendered with conda-smithy 3.52.3 and conda-forge-pinning 2025.10.24.23.37.09
    
    Other tools:
    - conda-build 25.9.0
    - rattler-build 0.48.1
    - rattler-build-conda-compat 1.4.9
    
    * MNT: Re-rendered with conda-smithy 3.52.3 and conda-forge-pinning 2025.10.24.23.37.09
    
    Other tools:
    - conda-build 25.9.0
    - rattler-build 0.48.1
    - rattler-build-conda-compat 1.4.9
    
    * MNT: Re-rendered with conda-smithy 3.52.3 and conda-forge-pinning 2025.10.24.23.37.09
    
    Other tools:
    - conda-build 25.9.0
    - rattler-build 0.48.1
    - rattler-build-conda-compat 1.4.9
    
    * MNT: Re-rendered with conda-smithy 3.52.3 and conda-forge-pinning 2025.10.24.23.37.09
    
    Other tools:
    - conda-build 25.9.0
    - rattler-build 0.48.1
    - rattler-build-conda-compat 1.4.9
    
    * MNT: Re-rendered with conda-smithy 3.52.3 and conda-forge-pinning 2025.10.27.09.15.48
    
    Other tools:
    - conda-build 25.9.0
    - rattler-build 0.48.1
    - rattler-build-conda-compat 1.4.9
    
    ---------
    
    Co-authored-by: David Dubbeldam <d.dubbeldam@uva.nl>

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,18 +2,31 @@
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
   if [[ "${target_platform}" == linux* ]]; then
-    cmake -B build --preset=linux_conda_raspa3 -DCMAKE_POLICY_VERSION_MINIMUM=3.32
+    cmake -B build --preset=linux_conda_raspa3_gcc -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   elif [[ "${target_platform}" == osx-* ]]; then
     cmake -B build --preset=mac_conda_raspa3 -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   fi
 else
   if [[ "${target_platform}" == linux-aarch64 ]]; then 
-    cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS}  -DCMAKE_CXX_COMPILER_AR="$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-ar" -DCMAKE_CXX_COMPILER_RANLIB="$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-ranlib" -DCMAKE_POLICY_VERSION_MINIMUM=3.32
+    cmake -B build --preset=linux_conda_raspa3_gcc ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DBUILD_APP=true -DBUILD_CLI=false -DBUILD_TESTING=false
   elif [[ "${target_platform}" == linux-ppc64le ]]; then
-    cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_CXX_COMPILER_AR="$BUILD_PREFIX/bin/powerpc64le-conda-linux-gnu-ar" -DCMAKE_CXX_COMPILER_RANLIB="$BUILD_PREFIX/bin/powerpc64le-conda-linux-gnu-ranlib" -DCMAKE_POLICY_VERSION_MINIMUM=3.32
+    cmake -B build --preset=linux_conda_raspa3_gcc ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DBUILD_APP=true -DBUILD_CLI=false -DBUILD_TESTING=false
   elif  [[ "${target_platform}" == osx-* ]]; then
     cmake -B build --preset=mac_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   fi
 fi
 
 ninja -C build install -v
+
+# second stage for linux cross-compilation
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+  if [[ "${target_platform}" == linux-aarch64 ]]; then 
+    rm -rf build
+    cmake -B build --preset=linux_conda_raspa3_gcc ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DBUILD_APP=false -DBUILD_CLI=true -DBUILD_TESTING=false
+    ninja -C build install -v
+  elif [[ "${target_platform}" == linux-ppc64le ]]; then
+    rm -rf build
+    cmake -B build --preset=linux_conda_raspa3_gcc ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DBUILD_APP=false -DBUILD_CLI=true -DBUILD_TESTING=false
+    ninja -C build install -v
+  fi
+fi
